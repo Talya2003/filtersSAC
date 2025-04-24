ea403064b684a9aa0943d8a38b37c1fd7888ee99
Talya <talya8399@gmail.com>
2025-04-24 13:27:06 +0300

    commit for changes in app to github pages

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import MyDashboard from './MyDashboard';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={
-            <header className="App-header">
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          } />
-          <Route path="/dashboard" element={<MyDashboard />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <iframe 
+        src="https://piba-qa.il30.hcs.cloud.sap/sap/fpa/ui/app.html#/story2&/s2/A557D9EB3FC671CA6AD5039E942D1022/?mode=present" 
+        width="100%" 
+        height="600px" 
+        title="Iframe Example" 
+        frameBorder="0"
+      />
+    </div>
   );
 }
 
