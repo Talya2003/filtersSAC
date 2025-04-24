4d8b36776243dc5d2c883ae12d8a5a2099af9425
Talya <talya8399@gmail.com>
2025-04-24 15:09:58 +0300

    fix dashboard size

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div>
       <iframe 
         src="https://piba-qa.il30.hcs.cloud.sap/sap/fpa/ui/app.html#/story2&/s2/A557D9EB3FC671CA6AD5039E942D1022/?mode=present" 
-        width="100%" 
-        height="600px" 
+        width="50%" 
+        height="100px" 
         title="Iframe Example" 
         frameBorder="0"
       />
