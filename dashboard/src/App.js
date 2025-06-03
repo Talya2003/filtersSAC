56c7292cca96a8d977687f2e24d6ffb4d2af9088
Talya <talya8399@gmail.com>
2025-06-03 11:13:33 +0300

    remove first button to the dashboard

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,23 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react'; 
 import './App.css';
-import MyDashboard from './MyDashboard'; 
+import MyDashboard from './MyDashboard';
 
 function App() {
-  const [showDashboard, setShowDashboard] = useState(false);
-
   return (
     <div className="app-wrapper">
-      {showDashboard ? (
-        <MyDashboard goHome={() => setShowDashboard(false)} />
-      ) : (
-        <div className="start-page">
-          <h1>ברוכה הבאה!</h1>
-          <p>כדי להתחיל, לחצי על הכפתור למטה</p>
-          <button onClick={() => setShowDashboard(true)} className="start-button">
-            עבור לדשבורד שלי
-          </button>
-        </div>
-      )}
+      <MyDashboard />
     </div>
   );
 }
